effb0b0a3f77104681fb16dee276956cf6261c6a
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-06-27 20:22:06 +0200

    correction

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+// exercice 1 //
+// $age = 27;
+// if($age >= 18){
+//     echo 'vous etes majeur';
+// } else {
+//     echo 'vous etes mineur';
+// }
+
+
+// exercice 2 //
+// $isEasy = true;
+// if($isEasy){
+//     echo "c'est trop facile";
+// } else {
+//     echo 'C\'est difficile !!!';
+// }
+
+// exercice 3 //
+// $age = 27;
+// $gender = 'Homme';
+
+// if($gender === 'Homme'){
+//     echo 'Vous êtes un homme ';
+//     if($age >= 18){
+//         echo 'et vous êtes majeur';
+//     } else {
+//         echo 'et vous êtes mineur'; 
+//     }
+
+// } else {
+//     echo 'Vous êtes une femme ';
+//     if($age >= 18){
+//         echo 'et vous êtes majeur';
+//     } else {
+//         echo 'et vous êtes mineur'; 
+//     }
+// }
+
+
+// exercice 4 //
+// $magnitude = rand(1,9);
+// $tabMagnitude = [
+//     'Micro-séisme impossible à ressentir.',
+//     'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.',
+//     'Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.',
+//     'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.',
+//     'Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.',
+//     'Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.',
+//     'Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.',
+//     'Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.',
+//     'Séisme capable de tout détruire sur une très vaste zone.'
+// ];
+
+// echo $tabMagnitude[$magnitude-1];
+
+
+// exercice 5 //
+// if($gender != 'Homme'){
+//     echo 'c\'est une développeuse !!!';
+// }else{
+//     echo 'C\'est un développeur !!!!';
+// }
+
+// exercice 6 //
+// if($age >= 18){
+//     echo 'Tu es majeur';
+// }else{
+//     echo 'Tu n\'es pas majeur';
+// }
+
+// exercice 7 //
+// if($isOk == false){
+//     echo 'c\'est pas bon !!!';
+// }else{
+//     echo 'c\'est ok !!!';
+// }
+
+// exercice 8 //
+// if($isOk){
+//     echo 'c\'est ok !!!';  
+// }else{
+//     echo 'c\'est pas bon !!!';
+// }
+
+?>
